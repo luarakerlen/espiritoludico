b1ea8143bec61ddab72038d7cb288df3ab428725
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2026-01-20 19:12:47 -0300

    feat: adjust por que alugar

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,28 @@
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
-import ComoFunciona from "./components/ComoFunciona/ComoFunciona";
-import PorQueAlugar from "./components/PorQueAlugar/PorQueAlugar";
-import Categorias from "./components/Categorias/Categorias";
-import QuemSomos from "./components/QuemSomos/QuemSomos";
-import ComunidadeEventos from "./components/ComunidadeEventos/ComunidadeEventos";
-import Footer from "./components/Footer/Footer";
-import styles from "./page.module.css";
-import Instagram from "./components/Instagram/Instagram";
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import ComoFunciona from './components/ComoFunciona/ComoFunciona';
+import PorQueAlugar from './components/PorQueAlugar/PorQueAlugar';
+import Categorias from './components/Categorias/Categorias';
+import QuemSomos from './components/QuemSomos/QuemSomos';
+import ComunidadeEventos from './components/ComunidadeEventos/ComunidadeEventos';
+import Footer from './components/Footer/Footer';
+import styles from './page.module.css';
+import Instagram from './components/Instagram/Instagram';
 
 export default function Home() {
-  return (
-    <div className={styles.home}>
-      <Header />
-      <main className={styles.main}>
-        <Hero />
-        <ComoFunciona />
-        <PorQueAlugar />
-        <Categorias />
-        <QuemSomos />
-        <ComunidadeEventos />
-      </main>
-      <Instagram />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className={styles.home}>
+			<Header />
+			<main className={styles.main}>
+				<Hero />
+				<ComoFunciona />
+				<PorQueAlugar />
+				<Categorias />
+				<ComunidadeEventos />
+				<QuemSomos />
+				{/* <Instagram /> */}
+			</main>
+			<Footer />
+		</div>
+	);
 }
